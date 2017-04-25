e484e628aa8c6388677c6c4bc36e6dc41763d2af
Josh Adams <josh.rubyist@gmail.com>
2017-04-25 16:16:14 -0500

    Add prettier for nicer js formatting

--- src/reducer.js
+++ src/reducer.js
@@ -1,29 +1,33 @@
 // @flow
 
-import { FormType, SectionType } from './types'
-import { List } from 'immutable'
+import { FormType, SectionType } from "./types";
+import { List } from "immutable";
 
-const init = new FormType()
+const init = new FormType();
 
 // FIXME: Remove this eventually but for now it makes it easy to do some testing
+const firstSection = new SectionType({
+  title: "First",
+  order: 1
+});
+const secondSection = new SectionType({ title: "Second", order: 2 });
 const exampleForm = new FormType({
-  sections: List([
-    new SectionType({ title: "Second", order: 0 }),
-    new SectionType({ title: "First", order: 1 })
-  ])
-})
+  sections: List([secondSection, firstSection])
+});
+// END EXAMPLE FORM DATA
 
-type Action =
-  'INCREMENT' |
-  'LOAD_EXAMPLE_FORM'
+type Action = "INCREMENT" | "LOAD_EXAMPLE_FORM";
 
-export default function reducer(form:FormType = init, action:{ type: Action }) {
-  switch(action.type) {
-    case 'LOAD_EXAMPLE_FORM':
-      return exampleForm
-    case 'INCREMENT':
-      return form.set('count', form.get('count') + 1)
+export default function reducer(
+  form: FormType = init,
+  action: { type: Action }
+) {
+  switch (action.type) {
+    case "LOAD_EXAMPLE_FORM":
+      return exampleForm;
+    case "INCREMENT":
+      return form.set("count", form.get("count") + 1);
     default:
-      return form
+      return form;
   }
 }
