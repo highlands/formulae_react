7142b5cd70c1da2c81c514470dc63a5aefdb2983
Josh Adams <josh.rubyist@gmail.com>
2017-05-10 10:27:35 -0500

    Add a test for section content paragraph

--- src/types/FormType.js
+++ src/types/FormType.js
@@ -6,10 +6,8 @@ import SectionType from "./SectionType";
 export default class FormType
   extends Record({
     id: 0,
-    sections: List(),
-    count: 0
+    sections: List()
   }) {
   id: number;
   sections: List<SectionType>;
-  count: number;
 }
