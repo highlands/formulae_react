4a17ebe317dc40516146058123ee0689f2cf8523
Franzé Jr <franzejr@gmail.com>
2017-07-22 09:28:15 -0300

    Fix flow issues and add a missing th for choices

--- src/output_lib/components/AdministerForm/ChoicesAdmin.js
+++ src/output_lib/components/AdministerForm/ChoicesAdmin.js
@@ -77,6 +77,7 @@ export default function ChoicesAdmin(props: Props) {
       <table className="pure-table">
         <thead>
           <tr>
+            <th />
             <th>
               Name
             </th>
